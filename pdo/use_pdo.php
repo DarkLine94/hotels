5fc62c31b94a479d0e51acf379ecd99a97cdaf12
DarkLine94 <tranlenguyen1109@gmail.com>
2020-02-17 03:11:36 +0000

    20200217

---
+++ pdo/use_pdo.php
@@ -0,0 +1,19 @@
+<?php
+$dsn = "mysql:host=localhost;dbname=hoteldb";
+$user = "hoteldb_admin";
+$password = "admin123";
+
+
+$pdo = null;
+try {
+	
+	$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET CHARACTER SET 'utf8'");
+	
+	$pdo = new PDO($dsn, $user, $password, $options);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	
+	echo $e->getMessage();
+}
+
+?>
